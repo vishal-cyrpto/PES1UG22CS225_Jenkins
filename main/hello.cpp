ea8d1796160edd945daef038ca482cec0d1de202
vishal-cyrpto <gummadi.vishal321@gmail.com>
2025-03-13 13:28:00 +0530

    Update hello.cpp message for Jenkins and Docker

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "Hello, Jenkins and Docker \n";
+  cout << "I have successfully built and run this YES \n";
   return 0;
 }
